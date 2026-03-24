ffee603b6b8cc25a113079f96431665f0d1ec41f
lamiera70 <lamiera70@yahoo.it>
2026-03-24 11:11:36 +0100

    reset copia

--- app-lista-bootstrap-due_inizio/src/components/ItemList/ItemList.jsx
+++ app-lista-bootstrap-due_inizio/src/components/ItemList/ItemList.jsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
+
 import "./ItemList.css";
 
 export default function ItemList({items}) {
 
-  const [isEdit, setIsEdit] = useState(false)
-
-
-  function handleEdit() {
-    setIsEdit(!isEdit)
-  }
-
-  
+    
 
   return (
 
@@ -22,32 +15,7 @@ export default function ItemList({items}) {
 
             items.map((item) => (
     
-              isEdit ? (
-
-                   <li 
-                  key={item.id}
-                  className="list-group-item d-flex justify-content-between align-items-center"
-                >
-                  <input className="form-check-input me-2" type="checkbox" value="" />
-                  
-                  {item.testo}
-      
-      
-                  <button 
-                    type="button"
-                    className="btn btn-info"
-                    onClick={handleEdit}
-                  >salva</button>
-
-                  <button 
-                    type="button"
-                    className="btn btn-info"
-                    onClick={handleEdit}
-                  >annulla</button>
-                </li>
-
-              ) : (
-
+            
                 <li 
                   key={item.id}
                   className="list-group-item d-flex justify-content-between align-items-center"
@@ -60,10 +28,9 @@ export default function ItemList({items}) {
                   <button 
                     type="button"
                     className="btn btn-info"
-                    onClick={handleEdit}
                   >modifica</button>
                 </li>
-              )
+              
               
     
             ))}
